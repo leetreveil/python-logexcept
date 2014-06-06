75c1e71d79be2b429025308d86c5e011e1d6cac0
Lee Treveil <leetreveil@gmail.com>
2014-06-06 13:18:01 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='logexcept',
+    version='0.0.1',
+    py_modules=['logexcept'],
+    author='Lee Treveil',
+    author_email='leetreveil@gmail.com',
+    license='MIT',
+    long_description=open('README.rst').read(),
+    url='http://github.org/leetreveil/logexcept/',
+)
