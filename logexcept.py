3cf2e13ea2dd562122507af20caf57dbc253feea
Lee Treveil <leetreveil@gmail.com>
2014-06-07 19:18:14 +0100

    refactorings

--- logexcept.py
+++ logexcept.py
@@ -9,23 +9,42 @@ def create_exchook(file=sys.stderr):
 
     def exchook(etype, value, tb):
 
-        def _get_global_loggers_from_frame(frame):
+        def _get_global_loggers_from_frame(frame_globals):
             # TODO: optimze by looking for common variable
             #       names first, e.g. 'logger' or 'LOGGER'
-            return [frame.f_globals[x] for x in frame.f_globals
-                if isinstance(frame.f_globals[x], logging.Logger)]
+            return [obj for (name, obj) in frame_globals.items()
+                if isinstance(obj, logging.Logger)]
+
+        def _create_log_lines(logs):
+            log_msg = '\n    logs:\n'
+            for pos, log_record in enumerate(logs):
+                log_msg += ' ' * 8
+                log_msg += 'line %d, msg: %s' % (log_record.lineno, handler.format(log_record))
+                if pos != len(logs) -1:
+                    log_msg += '\n'
+            return log_msg
+
+        def _extract_traceback(tb):
+            # copying what traceback.extract_tb does internally
+            f = tb.tb_frame
+            co = f.f_code
+            linecache.checkcache(co.co_filename)
+            line = linecache.getline(co.co_filename, tb.tb_lineno, tb.tb_frame.f_globals)
+            line = line.strip() if line else ''
+            return (co.co_filename, tb.tb_lineno, co.co_name, line)
 
         if hasattr(sys, 'tracebacklimit'):
             limit = sys.tracebacklimit
         else:
             limit = None
+
         list = []
         n = 0
         while tb is not None and (limit is None or n < limit):
             frame = tb.tb_frame
 
             handlers = []
-            global_loggers = _get_global_loggers_from_frame(frame)
+            global_loggers = _get_global_loggers_from_frame(frame.f_globals)
             for logger in global_loggers:
                 handlers += [x for x in logger.handlers if isinstance(x, LogCollector)]
 
@@ -35,30 +54,14 @@ def create_exchook(file=sys.stderr):
             except IndexError:
                 pass
 
-            # copying what extract_tb does internally
-            f = tb.tb_frame
-            lineno = tb.tb_lineno
-            co = f.f_code
-            filename = co.co_filename
-            name = co.co_name
-            linecache.checkcache(filename)
-            line = linecache.getline(filename, lineno, f.f_globals)
-            if line: line = line.strip()
-            else: line = ''
+            (filename, lineno, fn_name, line) = _extract_traceback(tb)
 
             if handler:
-                logs = [x for x in handler.logs if x.funcName == co.co_name]
+                logs = [x for x in handler.logs if x.funcName == fn_name]
                 if len(logs) > 0:
-                    log_msg = '\n    logs:\n'
-                    for pos, log_record in enumerate(logs):
-                        log_msg += ' ' * 8
-                        log_msg += 'line %d, msg: %s' % (log_record.lineno, handler.format(log_record))
-                        if pos != len(logs) -1:
-                            log_msg += '\n'
-                    line += log_msg
+                    line += _create_log_lines(logs)
 
-            list.append((filename, lineno, name, line))
-
+            list.append((filename, lineno, fn_name, line))
             tb = tb.tb_next
             n = n+1
 
