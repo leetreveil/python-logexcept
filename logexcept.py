1ef257dd1c693d301d57d222862750a5c9126a39
Lee Treveil <leetreveil@gmail.com>
2014-06-17 19:02:03 +0100

    make logs limited globally

--- logexcept.py
+++ logexcept.py
@@ -4,22 +4,18 @@ import logging
 import sys
 import traceback
 
+LOG_LIMIT = 1000
+global_logs = deque(maxlen=LOG_LIMIT)
 
 def create_exchook(file=sys.stderr, max_lines_per_function=25):
 
     def exchook(etype, value, tb):
 
-        def _get_global_loggers_from_frame(frame_globals):
-            # TODO: optimze by looking for common variable
-            #       names first, e.g. 'logger' or 'LOGGER'
-            return [obj for (name, obj) in frame_globals.items()
-                if isinstance(obj, logging.Logger)]
-
         def _create_log_lines(logs):
             log_msg = '\n    logs:\n'
-            for pos, log_record in enumerate(logs):
+            for pos, (log_record, log_handler) in enumerate(logs):
                 log_msg += ' ' * 8
-                log_msg += 'line %d, msg: %s' % (log_record.lineno, handler.format(log_record))
+                log_msg += 'line %d, msg: %s' % (log_record.lineno, log_handler.format(log_record))
                 if pos != len(logs) -1:
                     log_msg += '\n'
             return log_msg
@@ -41,25 +37,13 @@ def create_exchook(file=sys.stderr, max_lines_per_function=25):
         list = []
         n = 0
         while tb is not None and (limit is None or n < limit):
-            frame = tb.tb_frame
-
-            handlers = []
-            global_loggers = _get_global_loggers_from_frame(frame.f_globals)
-            for logger in global_loggers:
-                handlers += [x for x in logger.handlers if isinstance(x, LogCollector)]
-
-            handler = None
-            try:
-                handler = handlers[0]
-            except IndexError:
-                pass
-
             (filename, lineno, fn_name, line) = _extract_traceback(tb)
 
-            if handler:
-                logs = [x for x in handler.logs if x.funcName == fn_name][-max_lines_per_function:]
-                if len(logs) > 0:
-                    line += _create_log_lines(logs)
+            logs = [(record, handler) for (record, handler) in global_logs
+                if record.funcName == fn_name and record.pathname == filename]
+            logs = logs[-max_lines_per_function:]
+            if len(logs) > 0:
+                line += _create_log_lines(logs)
 
             list.append((filename, lineno, fn_name, line))
             tb = tb.tb_next
@@ -68,20 +52,19 @@ def create_exchook(file=sys.stderr, max_lines_per_function=25):
         file.write('Traceback (most recent call last):\n')
         map(file.write, traceback.format_list(list))
         map(file.write, traceback.format_exception_only(etype, value))
-
-        handler.logs.clear()
+        global_logs.clear()
 
     return exchook
 
 
 class LogCollector(logging.Handler):
 
-    def __init__(self, log_limit=1000):
+    def __init__(self):
         logging.Handler.__init__(self)
-        self.logs = deque(maxlen=log_limit)
 
     def emit(self, record):
         try:
-            self.logs.append(record)
+            global global_logs
+            global_logs.append((record, self))
         except:
             self.handleError(record)
